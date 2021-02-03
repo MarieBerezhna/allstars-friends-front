688d93c076545842e9fdf832fb1adff4c71c413a
MarieBerezhna <admin@webmarie.com>
2021-02-04 01:32:27 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container">
+          <div id="nav">
+        <router-link to="/">Home</router-link>
+      </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+        <router-view class="view"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
