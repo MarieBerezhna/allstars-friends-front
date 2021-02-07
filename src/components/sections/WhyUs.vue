1456df4e52d0ec7b40075073431e7def1a72f020
weirdox <marie.berezhna@gmail.com>
2021-02-08 00:33:54 +0200

    deploy

---
+++ src/components/sections/WhyUs.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container-fluid bg-light pt-5">
+<h2 class="font-size-bold w-100 text-center my-5">Why Work With Us</h2>
+   <div class="container pt-3">
+       <div class="row">
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card1.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           AllStarsIT is among TOP-10 IT companies in Kyiv according to anonymous voting on DOU 
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card2.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           Get a personal career growth plan along with learning activities (languages, professional skills, soft skills, and personal growth) 
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card3.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           Choose a stable and award-winning project in cybersecurity or fintech domain 
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow mt-lg-4">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card4.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           Remote work with flexible hours allows you to manage your schedule in the most convenient way
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow mt-lg-4">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card5.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           Medical insurance, sports activities, and educational compensations for your life-work balance
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="col-12 col-lg-4 why-card-wrapper">
+               <div class="row card-inner m-2 p-2 pt-3 bg-white border-radius box-shadow mt-lg-4">
+                   <div class="col-12">
+                       <img :src="require('@/assets/why-card5.png')" alt="" width="30%" height="auto">
+                   </div>
+                   <div class="col-12">
+                       <p>
+                           Innovative corporate culture is a leading priority for our company
+                       </p>
+                   </div>
+               </div>
+           </div>
+       </div>
+
+   </div>
+   
+    </div>
+</template>
+<style  scoped>
+.card-inner {
+    height: auto;
+    min-height: 300px;
+}
+</style>
