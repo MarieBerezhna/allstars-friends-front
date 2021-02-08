00019171b54afd3cc860a603102de52dcb88f176
weirdox <marie.berezhna@gmail.com>
2021-02-08 02:02:25 +0200

    deploy

---
+++ src/components/sections/JobOpenings.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container-fluid bg-white">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
